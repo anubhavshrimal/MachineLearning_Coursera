2651318122efdbed4d81bcd1d904985b7ca76596
anubhavshrimal <anubhavshrimal@gmail.com>
2016-09-23 23:19:43 +0530

    modified ex1_multi.m with testing features

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -104,7 +104,17 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+        features = [1650, 3];
+        
+        for i = 1 : length(features)
+        
+            features(i) = (features(i) - mu(i)) / sigma(i);
+        
+        end
+        
+        features = [ones(1,1) features];
+        price = features * theta; % You should change this
+
 
 
 % ============================================================
@@ -149,7 +159,16 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+        features = [1650, 3];
+        
+        for i = 1 : length(features)
+        
+        features(i) = (features(i) - mu(i)) / sigma(i);
+        
+        end
+        
+        features = [ones(1,1) features];
+        price = features * theta; % You should change this
 
 
 % ============================================================
